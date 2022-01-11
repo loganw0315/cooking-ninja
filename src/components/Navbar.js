b14b4d20927d23c9a5bcacfcdccc93ffb85bdf64
Logan Willis <loganw0315@gmail.com>
2022-01-11 12:13:53 -0500

    added useContext and useReducer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,14 @@
-import { useContext } from 'react'
 import { Link } from 'react-router-dom'
-import { ThemeContext } from '../context/ThemeContext'
 import './Navbar.css'
 import Searchbar from './Searchbar'
+import { useTheme } from '../hooks/useTheme' 
 
 export default function Navbar() {
-    const {color} = useContext(ThemeContext)
+    const {color, changeColor} = useTheme()
 
     return (
         <div className='navbar' style={{background: color}}>
-            <nav>
+            <nav onClick={() => changeColor('pink')}>
                 <Link to='/' className='brand'>
                     <h1>Cooking Ninja</h1>
                 </Link>
---
+++ src/components/ThemeSelector.js
@@ -0,0 +1,2 @@
+import './ThemeSelector.css'
+
